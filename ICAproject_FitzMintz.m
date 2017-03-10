ba8499d14613ecfcc5c5086d1549a8e40b4550e7
Katie Fitzsimons <fitzsi28@gmail.com>
2017-03-09 23:24:27 -0600

    correlation of observations added

--- ICAproject_FitzMintz.m
+++ ICAproject_FitzMintz.m
@@ -20,42 +20,34 @@ When you run c
 NumSig = 5;
 NumObs = 8;
 %info = audioinfo('record_Al.m4a')
-[Al,Fs] = audioread('record_Al.m4a');
+[asyncmix{1},Fs] = audioread('record_Al.m4a');
 [p,q] = rat(16000/Fs,0.0001);
-Al = resample(Al,p,q);
-[ash,Fs] = audioread('record_ASH.m4a');
+asyncmix{1} = resample(asyncmix{1},p,q);
+[asyncmix{2},Fs] = audioread('record_ASH.m4a');
 [p,q] = rat(16000/Fs,0.0001);
-ash = resample(ash,p,q);
-[cc,Fs] = audioread('record_CC.m4a');
+asyncmix{2} = resample(asyncmix{2},p,q);
+[asyncmix{3},Fs] = audioread('record_CC.m4a');
 [p,q] = rat(16000/Fs,0.0001);
-cc = resample(cc,p,q);
-[kf,Fs] = audioread('record_kf.mp3');
+asyncmix{3} = resample(asyncmix{3},p,q);
+[asyncmix{4},Fs] = audioread('record_kf.mp3');
 [p,q] = rat(16000/Fs,0.0001);
-kf = resample(kf,p,q);
-[mw,Fs] = audioread('record_mw.m4a');
+asyncmix{4} = resample(asyncmix{4},p,q);
+[asyncmix{5},Fs] = audioread('record_mw.m4a');
 [p,q] = rat(16000/Fs,0.0001);
-mw = resample(mw,p,q);
-[my,Fs] = audioread('record_MY.m4a');
+asyncmix{5} = resample(asyncmix{5},p,q);
+[asyncmix{6},Fs] = audioread('record_MY.m4a');
 [p,q] = rat(16000/Fs,0.0001);
-my = resample(my,p,q);
-[mb,Fs] = audioread('record_MB.m4a');
+asyncmix{6} = resample(asyncmix{6},p,q);
+[asyncmix{7},Fs] = audioread('record_MB.m4a');
 [p,q] = rat(16000/Fs,0.0001);
-mb = resample(mb,p,q);
-[tony,Fs] = audioread('record_Tony.m4a');
+asyncmix{7} = resample(asyncmix{7},p,q);
+[asyncmix{8},Fs] = audioread('record_Tony.m4a');
 [p,q] = rat(16000/Fs,0.0001);
-tony = resample(tony,p,q);
-asyncmix{1} = Al;
-asyncmix{2} = ash;
-asyncmix{3} = cc;
-asyncmix{4} = kf;
-asyncmix{5} = mw;
-asyncmix{6} = my;
-asyncmix{7} = mb;
-asyncmix{8} = tony;
+asyncmix{8} = resample(asyncmix{8},p,q);
 
 leadlag = zeros(NumObs,1);%NumObs);
 lengths = zeros(NumObs,1);
-noiseratio = zeros(Numobs,1);
+noiseratio = zeros(NumObs,1);
 for i=1:NumObs;
     lengths(i) = length(asyncmix{i});
     noiseratio(i) = snr(asyncmix{i});
@@ -75,20 +67,20 @@ for i=1:NumObs;
     end
     newlengths(i) = length(syncmix{i});
 end
-%{
+clear laglist;
+
 synclag = zeros(NumObs,NumObs);
+syncorr= zeros(NumObs,NumObs);
 for i=1:NumObs;
     %lengths(i) = length(asyncmix{i});
     for j=1:NumObs;
     [corrlist,laglist]=xcorr(syncmix{i},syncmix{j});
     [maxcorr,maxind] = max(corrlist);
     synclag(i,j) = laglist(maxind);
+    syncorr(i,j) = maxcorr/1000;
     end
 end
 
-[M,I] = min(leadlag(:));
-[I_row,I_col] = ind2sub(size(leadlag),I);
-%}
 newend = min(newlengths);
 
 mix = zeros(newend,0);
@@ -96,10 +88,9 @@ for j=1:NumObs;
     mix = [mix,syncmix{j}(1:newend)];
 end
 mix = mix.';
-similarity = corrcoef(mix)
+mix = mix(2:7,:);%remove first because we could not align and last because it was strongly correlated with 3 and 7
 
-
-NumSamps = NumObs;%length(mix(:,1));
+NumSamps = length(mix(:,1));
 t=linspace(0,newend/16000,newend);
 
 start= 0; %for debugging purposes only
@@ -116,6 +107,7 @@ figure(1)
 xlabel('Time')
 figure(1)
 suplabel('Audio Signal','y');
+clear asyncmix;
 
 for i=1:NumSamps;
     figure(2)
